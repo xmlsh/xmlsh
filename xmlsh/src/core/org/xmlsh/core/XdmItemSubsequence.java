f8300cf16fd9ef533faa9adde1721ac1cac8ae09
David Lee <dlee@calldei.com>
2009-07-27 01:08:05 +0000

    Missing checkin for shift command

---
+++ xmlsh/src/core/org/xmlsh/core/XdmItemSubsequence.java
@@ -0,0 +1,62 @@
+/**
+ * $Id: $
+ * $Date: $
+ *
+ */
+
+package org.xmlsh.core;
+
+import java.util.Iterator;
+
+import net.sf.saxon.s9api.XdmItem;
+
+public class XdmItemSubsequence implements Iterable<XdmItem> {
+
+	Iterable<XdmItem>	mIter;
+	int		mBegin;
+	
+	
+	@Override
+	public Iterator<XdmItem> iterator() {
+		Iterator<XdmItem> iter = mIter.iterator();
+		for( int i = 0 ; i < mBegin ; i++ )
+			if( iter.hasNext() )
+				iter.next();
+		return iter ;
+		
+		
+	}
+
+	XdmItemSubsequence(Iterable<XdmItem> i, int begin)
+	{
+		mIter = i;
+		mBegin = begin;
+		
+	}
+	
+	
+
+}
+
+
+
+//
+//
+//Copyright (C) 2008,2009 David A. Lee.
+//
+//The contents of this file are subject to the "Simplified BSD License" (the "License");
+//you may not use this file except in compliance with the License. You may obtain a copy of the
+//License at http://www.opensource.org/licenses/bsd-license.php 
+//
+//Software distributed under the License is distributed on an "AS IS" basis,
+//WITHOUT WARRANTY OF ANY KIND, either express or implied.
+//See the License for the specific language governing rights and limitations under the License.
+//
+//The Original Code is: all this file.
+//
+//The Initial Developer of the Original Code is David A. Lee
+//
+//Portions created by (your name) are Copyright (C) (your legal entity). All Rights Reserved.
+//
+//Contributor(s): none.
+//
