68ef992c13b9108658cb176e7300ae502556bed7
David Lee <dlee@calldei.com>
2012-05-30 17:59:52 +0000

    First checkpoint with a working XdmItem  xpipe. Test:
    xecho <[ 1, <a/> , "str" ]>  | while xread a ; do echo $a done

---
+++ xmlsh/src/core/org/xmlsh/core/XdmStreamInputPort.java
@@ -0,0 +1,213 @@
+/**
+ * $Id: $
+ * $Date: $
+ *
+ */
+
+package org.xmlsh.core;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import javax.xml.stream.XMLEventReader;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+import javax.xml.transform.Source;
+
+import net.sf.saxon.Configuration;
+import net.sf.saxon.evpull.Decomposer;
+import net.sf.saxon.evpull.EventToStaxBridge;
+import net.sf.saxon.om.NodeInfo;
+import net.sf.saxon.s9api.SaxonApiException;
+import net.sf.saxon.s9api.XdmItem;
+import net.sf.saxon.s9api.XdmNode;
+import net.sf.saxon.s9api.XdmValue;
+import org.xml.sax.InputSource;
+import org.xmlsh.sh.shell.SerializeOpts;
+import org.xmlsh.sh.shell.Shell;
+import org.xmlsh.util.NullInputStream;
+import org.xmlsh.util.S9Util;
+import org.xmlsh.util.Util;
+
+public class XdmStreamInputPort extends InputPort {
+
+	
+	IXdmValueReader  mReader;
+
+	public XdmStreamInputPort(IXdmValueReader reader, SerializeOpts opts) {
+		mReader = reader ;
+	}
+
+	@Override
+	public InputStream asInputStream(SerializeOpts opts) throws CoreException {
+		
+		XdmValue value = mReader.read();
+		if( value == null )
+			return new NullInputStream();
+		
+		
+		ByteArrayOutputStream buf = new ByteArrayOutputStream();
+		try {
+			Util.writeXdmValue(value, Util
+						.streamToDestination(buf, opts)); // uses output xml encoding
+			
+			return new ByteArrayInputStream(buf.toByteArray());
+		} catch (SaxonApiException e) {
+			throw new CoreException(e);
+		} 
+	}
+
+	@Override
+	public void close() throws CoreException {
+		
+
+	}
+
+	@Override
+	public Source asSource(SerializeOpts opts) throws CoreException {
+		return new XValue( mReader.read()).asSource();
+	}
+
+	@Override
+	public InputSource asInputSource(SerializeOpts opts) throws CoreException {
+		InputSource in = new InputSource(asInputStream(opts));
+		in.setSystemId(this.getSystemId());
+		return in;
+	}
+
+	@Override
+	public XdmNode asXdmNode(SerializeOpts opts) throws CoreException {
+		return  (XdmNode) mReader.read() ;
+	}
+
+	@Override
+	public void copyTo(OutputStream out, SerializeOpts opts) throws CoreException, IOException {
+		XdmItem item ;
+		while( ( item = mReader.read()) != null )
+			try {
+				Util.writeXdmValue(item, Util.streamToDestination(out, opts));
+			} catch (SaxonApiException e) {
+				throw new CoreException(e);
+			}
+
+
+	}
+
+	@Override
+	public XMLEventReader asXMLEventReader(SerializeOpts opts) throws CoreException {
+		try {
+			return XMLInputFactory.newInstance().createXMLEventReader(asXMLStreamReader(opts));
+		} catch (Exception e){
+			throw new CoreException(e);
+		}
+	}
+
+	@Override
+	public XMLStreamReader asXMLStreamReader(SerializeOpts opts) throws CoreException {
+		
+		// TODO: This code was copied from VariableInputPort 
+		
+         XValue value = new XValue( mReader.read() );
+		
+		//System.err.println("sysid: " + this.getSystemId() );
+		//System.err.println("base: " + value.asXdmNode().getBaseURI());
+	
+		Configuration config = Shell.getProcessor().getUnderlyingConfiguration();
+
+		/*
+		 * IF variable is an atomic value then treat as string and parse to XML
+		 */
+		
+		if( value.isAtomic() ){
+			
+			try {
+				XMLInputFactory factory = XMLInputFactory.newInstance();
+				if( ! opts.isSupports_dtd())
+					factory.setProperty(XMLInputFactory.SUPPORT_DTD, "false");
+				XMLStreamReader reader =  factory.createXMLStreamReader(getSystemId() , asInputStream(opts));
+				return reader;
+			} catch (Exception e)
+				{
+					throw new CoreException( e );
+				}
+			
+			
+		}
+		
+		
+								
+		// SequenceIterator iter = value.asSequenceIterator();
+		NodeInfo nodeInfo = value.asNodeInfo();
+		
+		/*
+		 * 2010-05-19 - EventReaders assume documents, if not a document then wrap with one
+		 */
+		if( nodeInfo.getNodeKind() != net.sf.saxon.type.Type.DOCUMENT )
+			nodeInfo = S9Util.wrapDocument( nodeInfo ).getUnderlyingNode(); ;
+	
+		
+		
+		
+		Decomposer decomposed = new Decomposer( nodeInfo , config.makePipelineConfiguration()  );
+		
+		// EventIteratorOverSequence eviter = new EventIteratorOverSequence(iter);
+		
+		
+		EventToStaxBridge ps = new EventToStaxBridge(	decomposed , config.makePipelineConfiguration() );
+		
+		
+		
+		// TODO: Bug in Saxon 9.1.0.6 
+		// PullToStax starts in state 0 not state START_DOCUMENT
+		if( ps.getEventType() == 0 )
+			try {
+				ps.next();
+			} catch (XMLStreamException e) {
+				throw new CoreException(e);
+			}
+		
+		
+		return ps;
+
+	}
+
+	@Override
+	public XdmItem asXdmItem(SerializeOpts serializeOpts) throws CoreException {
+		return (XdmItem) mReader.read();
+	}
+
+	// Default implementation uses a singleton as the input stream
+	public IXdmItemInputStream asXdmItemInputStream(SerializeOpts serializeOpts)
+			throws CoreException {
+		
+		return mReader ;
+	}
+	
+}
+
+
+
+//
+//
+//Copyright (C) 2008,2009,2010,2011 David A. Lee.
+//
+//The contents of this file are subject to the "Simplified BSD License" (the "License");
+//you may not use this file except in compliance with the License. You may obtain a copy of the
+//License at http://www.opensource.org/licenses/bsd-license.php 
+//
+//Software distributed under the License is distributed on an "AS IS" basis,
+//WITHOUT WARRANTY OF ANY KIND, either express or implied.
+//See the License for the specific language governing rights and limitations under the License.
+//
+//The Original Code is: all this file.
+//
+//The Initial Developer of the Original Code is David A. Lee
+//
+//Portions created by (your name) are Copyright (C) (your legal entity). All Rights Reserved.
+//
+//Contributor(s): none.
+//
---
+++ xmlsh/src/core/org/xmlsh/core/XdmStreamOutputPort.java
@@ -0,0 +1,180 @@
+/**
+ * $Id: $
+ * $Date: $
+ *
+ */
+
+package org.xmlsh.core;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
+
+import javanet.staxutils.ContentHandlerToXMLStreamWriter;
+import javanet.staxutils.XMLStreamEventWriter;
+
+import javax.xml.stream.XMLEventWriter;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamWriter;
+
+import net.sf.saxon.s9api.BuildingStreamWriter;
+import net.sf.saxon.s9api.Destination;
+import net.sf.saxon.s9api.Processor;
+import net.sf.saxon.s9api.SaxonApiException;
+import net.sf.saxon.s9api.XdmDestination;
+import net.sf.saxon.trans.XPathException;
+import org.xml.sax.ContentHandler;
+import org.xmlsh.sh.shell.SerializeOpts;
+import org.xmlsh.sh.shell.Shell;
+
+public class XdmStreamOutputPort extends OutputPort {
+ 
+	private IXdmValueWriter mWriter ;
+	
+	// Set to true if any asXXX method was caused which used a non-xml stream or access 
+	private		boolean		mAsText = false ;
+	
+	// Transient classes 
+	private		XdmDestination	 		mXdmDestination;
+	private		ByteArrayOutputStream 	mByteArrayOutputStream;
+	private		BuildingStreamWriter	mBuilder;
+	private		SerializeOpts 			mSerializeOpts; 	// for converting from ByteArray to string  
+	
+	
+	
+	public XdmStreamOutputPort(IXdmValueWriter writer, SerializeOpts opts) {
+		mWriter = writer;
+	}
+
+	@Override
+	public OutputStream asOutputStream(SerializeOpts opts) {
+		mSerializeOpts = opts ;
+		mAsText = true ;
+		return ( mByteArrayOutputStream = new ByteArrayOutputStream()); 	// BOS is synchroized 
+	}
+
+	@Override
+	public synchronized void flush() throws  CoreException, SaxonApiException
+	{
+			
+			
+			if (mXdmDestination != null)
+				mWriter.write( mXdmDestination.getXdmNode());
+
+			
+			// else
+			if (mByteArrayOutputStream != null)
+				try {
+					mWriter.write( new XValue(mByteArrayOutputStream.toString(mSerializeOpts.getOutputTextEncoding()   )).asXdmValue() );
+				} catch (UnsupportedEncodingException e1) {
+					throw new CoreException( e1 );
+				}
+
+			//else
+			if (mBuilder != null)
+				mWriter.write(mBuilder.getDocumentNode());
+				
+		
+			mXdmDestination = null;
+			mByteArrayOutputStream = null ;
+			mBuilder = null ;
+		
+	}
+	@Override
+	public void close() throws CoreException {
+		mWriter.close();
+
+	}
+
+	@Override
+	public synchronized PrintStream asPrintStream(SerializeOpts opts)
+	{
+		mAsText = true ;
+		return new PrintStream(asOutputStream(opts));
+	}
+
+	@Override
+	public synchronized Destination asDestination(SerializeOpts opts) throws InvalidArgumentException
+	{
+			// mVariable.clear();
+			mXdmDestination = new XdmDestination();
+		
+			return mXdmDestination;
+	}
+	
+
+	@Override
+	public void writeSequenceSeperator(SerializeOpts serializeOpts) throws IOException,
+			SaxonApiException, CoreException {
+		flush();
+
+	}
+
+	@Override
+	public void writeSequenceTerminator(SerializeOpts serializeOpts) throws IOException, CoreException, SaxonApiException {
+		flush();
+
+
+	}
+
+	@Override
+	public synchronized XMLStreamWriter asXMLStreamWriter(SerializeOpts opts) throws SaxonApiException {
+	
+		Processor proc = Shell.getProcessor();
+		BuildingStreamWriter bw = proc.newDocumentBuilder().newBuildingStreamWriter();
+		
+		
+		mBuilder = bw;
+		return bw;
+		
+		
+	}
+
+	@Override
+	public XMLEventWriter asXMLEventWriter(SerializeOpts opts) throws InvalidArgumentException,
+			 SaxonApiException {
+		XMLStreamWriter sw = asXMLStreamWriter(opts);
+		return new XMLStreamEventWriter( sw );
+		
+		
+	}
+
+	@Override
+	public IXdmItemOutputStream asXdmItemOutputStream(SerializeOpts opts) throws CoreException {
+		// TODO Auto-generated method stub
+		return mWriter;
+	}
+
+	@Override
+	public ContentHandler asContentHandler(SerializeOpts opts) throws XPathException,
+			SaxonApiException {
+		XMLStreamWriter sw = asXMLStreamWriter(opts);
+		return new ContentHandlerToXMLStreamWriter(sw);
+	}
+
+}
+
+
+
+//
+//
+//Copyright (C) 2008,2009,2010,2011 David A. Lee.
+//
+//The contents of this file are subject to the "Simplified BSD License" (the "License");
+//you may not use this file except in compliance with the License. You may obtain a copy of the
+//License at http://www.opensource.org/licenses/bsd-license.php 
+//
+//Software distributed under the License is distributed on an "AS IS" basis,
+//WITHOUT WARRANTY OF ANY KIND, either express or implied.
+//See the License for the specific language governing rights and limitations under the License.
+//
+//The Original Code is: all this file.
+//
+//The Initial Developer of the Original Code is David A. Lee
+//
+//Portions created by (your name) are Copyright (C) (your legal entity). All Rights Reserved.
+//
+//Contributor(s): none.
+//
