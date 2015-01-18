89abf5bbb2127c5d42b094ec2c96982ba65c260b
David Lee <dlee@calldei.com>
2015-01-17 22:01:56 -0800

    Fix up build files so ant buids in each componentn directory wrok better

---
+++ xmlsh/win32/xmlshdeb.bat
@@ -0,0 +1,15 @@
+@setlocal
+@echo off
+IF NOT DEFINED XMLSH GOTO NOX 
+SET CP=%XMLSH%\bin\*;%XMLSH%\lib\*
+SET _JVMDEFS=-Djavax.xml.parsers.SAXParserFactory=org.apache.xerces.jaxp.SAXParserFactoryImpl
+
+IF NOT DEFINED XMLSH_JVMOPTS SET XMLSH_JVMOPTS=-XX:+UseConcMarkSweepGC -Xmx1024m -Xms256m 
+set _JVMDEFS=%_JVMDEFS% -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=8888
+@echo on
+java %_JVMDEFS% -cp %CP% %XMLSH_JVMOPTS% org.xmlsh.sh.shell.Shell   %*
+exit /b 0
+
+:NOX
+echo XMLSH not defined
+exit /b 1
