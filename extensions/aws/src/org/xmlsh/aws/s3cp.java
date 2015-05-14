63dab818d0eb4dbb8b7bda2e70c95edd11ae1dbb
David Lee <dlee>
2015-05-14 07:46:06 -0400

    Added feature (-m) option to s3-cp which preserves metata - this is to fix ETAG of S3 object by doing a copy put on itself.

--- extensions/aws/src/org/xmlsh/aws/s3cp.java
+++ extensions/aws/src/org/xmlsh/aws/s3cp.java
@@ -10,14 +10,15 @@ import org.xmlsh.core.Options;
 import org.xmlsh.core.UnexpectedException;
 import org.xmlsh.core.XValue;
 
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.AccessControlList;
+import com.amazonaws.services.s3.model.CopyObjectRequest;
 import com.amazonaws.services.s3.model.CopyObjectResult;
+import com.amazonaws.services.s3.model.ObjectMetadata;
 
 
 public class s3cp extends AWSS3Command {
 
-
-
-	private		boolean 	mOnlyIfExists = false ;
 
 	/**
 	 * @param args
@@ -25,89 +26,79 @@ public class s3cp extends AWSS3Command {
 	 */
 	@Override
 	public int run(List<XValue> args) throws Exception {
-
-
-		Options opts = getOptions();
+		Options opts =  getOptions("m=metadata,a=acl,s=storage:,v=verbose,e=exists");
 		opts.parse(args);
+        args = opts.getRemainingArgs();
+	    String storage = opts.getOptString("storage", null);
 
-		args = opts.getRemainingArgs();
-
-
+	    boolean     onlyIfExists = opts.hasOpt("e");
+	    boolean     copyMetadata = opts.hasOpt("m");
+	    boolean     copyAcl = opts.hasOpt("acl");
+				
 		try {
-			getS3Client(opts);
+			 getS3Client(opts);
 		} catch (UnexpectedException e) {
 			usage( e.getLocalizedMessage() );
 			return 1;
-
+			
 		}
-
-
-
+		
 		if( args.size() != 2 ){
-			usage();
-			return 1;
+				usage();
+				return 1;
 		}
 
 		S3Path src = new S3Path(args.get(0).toString());
 		S3Path dest = new S3Path(args.get(1).toString());
-
+		
 		int ret = 0;
 		try {
-			ret = copy(  src , dest );
+			ret = copy(  src , dest , storage , onlyIfExists , copyAcl , copyMetadata);
 		} catch( Exception e ){
 			mShell.printErr("Exception copying " + src + " to " + dest , e);
 			ret = 1;
-
-
 		}
-
-
-
-
+		
 		return ret;
-
-
 	}
 
-
-	private int copy(S3Path src, S3Path dest) 
+	private int copy(S3Path src, S3Path dest,String storage, boolean onlyIfExists, boolean copyAcl, boolean copyMetadata) 
 	{
-
-
+		AmazonS3Client client = getAWSClient();
 		boolean bCopy = true ;
-
-
-		if( mOnlyIfExists ){
+		if( onlyIfExists ){
 			String destETag ;
 			String sourceETag ;
-			destETag = AWSUtil.getChecksum(getAWSClient() , dest ); 
-
+			destETag = AWSUtil.getChecksum( client  , dest ); 
+			
 			if( destETag != null  ){
-				sourceETag = AWSUtil.getChecksum( getAWSClient() , src  );
+				sourceETag = AWSUtil.getChecksum( client , src  );
 				if( sourceETag != null )
 					if( destETag.equals(sourceETag))
 						bCopy = false ;
 			}
+			if( ! bCopy )
+			    return 0;
 		}
-
-
-		if( bCopy ){
-
+		ObjectMetadata meta = copyMetadata ? client.getObjectMetadata(src.getBucket(), src.getKey()) : null ;
+        AccessControlList acl = copyAcl ? client.getObjectAcl(src.getBucket(), src.getKey()) : null ;
+        
 			traceCall("copyObject");
 
-			CopyObjectResult result = getAWSClient().copyObject(src.getBucket(), src.getKey(), dest.getBucket(), dest.getKey() );
-
+            CopyObjectRequest request = new CopyObjectRequest(src.getBucket(), src.getKey(), 
+			         dest.getBucket(), dest.getKey() );
+            if( meta != null )
+                request.setNewObjectMetadata(meta);
+            if( storage != null )
+                request.setStorageClass(storage);
+            if( acl != null )
+                request.setAccessControlList(acl);
+			
+			CopyObjectResult result = client.copyObject(request);
 			return 0;
-
-
-		}	else
-			return 0;
-
-
+			
 	}
 
-
-	@Override
 	public void usage() {
 		super.usage("Usage: s3cp source dest");
 	}
@@ -116,6 +107,6 @@ public class s3cp extends AWSS3Command {
 
 
 
-
+	
 
 }
