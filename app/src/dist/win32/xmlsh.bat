4256ffbfc4e5ce79316d9f1a298de626da0550d4
David Lee <dlee@nexstra.com>
2016-09-02 09:58:22 -0400

    Remove jline from windows startup

--- app/src/dist/win32/xmlsh.bat
+++ app/src/dist/win32/xmlsh.bat
@@ -4,6 +4,7 @@ IF NOT DEFINED XMLSH GOTO NOX
 SET CP=%XMLSH%\bin\*;%XMLSH%\lib\*
 SET _JVMDEFS=-Djavax.xml.parsers.SAXParserFactory=org.apache.xerces.jaxp.SAXParserFactoryImpl
 IF NOT DEFINED XMLSH_JVMOPTS SET XMLSH_JVMOPTS=-XX:+UseConcMarkSweepGC -Xmx1024m -Xms256m 
+SET _JVMDEFS=%_JVMDEFS% -Dxmlsh.jline=false
 java %_JVMDEFS% -cp %CP% %XMLSH_JVMOPTS% org.xmlsh.sh.shell.Shell   %*
 exit /b 0
 
